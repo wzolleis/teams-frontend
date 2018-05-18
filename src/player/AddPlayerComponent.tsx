30806c148e5b8553a0f216a40aa7ab2239c667bd
Wolfgang Zolleis <wzolleis@web.de>
2018-05-18 22:51:31 +0200

    add player form mit grid css

--- src/player/AddPlayerComponent.tsx
+++ src/player/AddPlayerComponent.tsx
@@ -47,73 +47,31 @@ class AddPlayerComponent extends React.Component<AddPlayerProps & AddPlayerDispa
 
     render() {
         return (
-            <div className="container">
-                <h3 className="text-center">Neuer Spieler</h3>
-                <form className="form-horizontal">
-
-
-                    <div className="form-group">
-                        <div className="col-md-4">
-                            <input
-                                value={this.state.name}
-                                onChange={this.handleChange}
-                                id="name"
-                                name="Name"
-                                type="text"
-                                placeholder="Name eingeben"
-                                className="form-control input-md"
-                            />
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <label className="col-md-4 control-label" htmlFor="skill_overall">Gesamt</label>
-                        <div className="col-md-4">
-                            <select
-                                value={this.state.overall}
-                                onChange={this.handleChange}
-                                id="skill_overall"
-                                name="skill_overall"
-                                className="form-control"
-                            >
-                                <option value="100">Voll krass</option>
-                                <option value="70">Ganz gut</option>
-                                <option value="50">brauchbar</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <label className="col-md-4 control-label" htmlFor="skill_ausdauer">Ausdauer</label>
-                        <div className="col-md-4">
-                            <select id="skill_ausdauer" name="skill_ausdauer" className="form-control">
-                                <option value="100">Marathon</option>
-                                <option value="70">Jogger</option>
-                                <option value="50">Beamter</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <label className="col-md-4 control-label" htmlFor="skill_speed">Speed</label>
-                        <div className="col-md-4">
-                            <select id="skill_speed" name="skill_speed" className="form-control">
-                                <option value="100">Porsche</option>
-                                <option value="70">Polo</option>
-                                <option value="50">Trabi</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <label className="col-md-4 control-label" htmlFor="skill_technik">Technik</label>
-                        <div className="col-md-4">
-                            <select id="skill_technik" name="skill_technik" className="form-control">
-                                <option value="100">Barcelona</option>
-                                <option value="70">Bayern</option>
-                                <option value="50">FCA</option>
-                            </select>
-                        </div>
-                    </div>
-                    <span className="submit-btn">
-                        <button className="btn btn-primary">Speichern</button>
-                </span>
+            <div>
+                <form className="container form-horizontal">
+                    <h3 className="text-center form-header">Neuer Spieler</h3>
+                    <label className="control-label form-label" htmlFor="name">Name</label>
+                    <input
+                        value={this.state.name}
+                        onChange={this.handleChange}
+                        id="name"
+                        name="Name"
+                        type="text"
+                        placeholder="Name eingeben"
+                        className="form-control form-input"
+                    />
+                    <label className="control-label form-label" htmlFor="skill_overall">Gesamt</label>
+                    <select
+                        value={this.state.overall}
+                        onChange={this.handleChange}
+                        id="skill_overall"
+                        name="skill_overall"
+                        className="form-control form-input"
+                    >
+                        <option value="100">Voll krass</option>
+                        <option value="70">Ganz gut</option>
+                        <option value="50">brauchbar</option>
+                    </select>
                 </form>
             </div>
         );
