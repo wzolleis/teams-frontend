d3774e68b3a735a647cdd9526d80b1c891dfcf9c
Wolfgang Zolleis <wzolleis@web.de>
2018-05-19 12:38:33 +0200

    backend url mit Umgebungsvariable

--- src/api/TeamsApi.ts
+++ src/api/TeamsApi.ts
@@ -1,11 +1,12 @@
 import axios, {AxiosInstance} from "axios";
 import {Player} from "../app/Types";
 
-const BASE_URL: string = 'https://teams-backend.herokuapp.com';
+const BASE_URL: string = process.env.TEAMS_BACKEND_URL || 'https://teams-backend-dev.herokuapp.com';
 
 export async function getPlayers(): Promise<Player[]> {
     try {
         // Set config defaults when creating the instance
+        console.log('connection to backend:', BASE_URL);
         const instance: AxiosInstance = axios.create({
             baseURL: BASE_URL
         });
